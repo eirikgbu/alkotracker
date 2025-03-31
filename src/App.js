2ce8ad9cd2b9bb43c9a6929636f0055acde1edfe
Eirik Buset <eirikgbu@stud.ntnu.no>
2025-03-31 16:02:28 +0100

    må få tak i noe gratis sheets API som funker

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 import React, { useEffect, useState } from "react";
 
 function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetch("https://api.sheetbest.com/sheets/9caec6ee-9d05-425e-9a9a-b391186629d0")
-      .then((res) => res.json())
-      .then((data) => setData(data));
-  }, []);
-
   return (
     <div>
-      <h1>Data fra Google Sheets</h1>
-      <ul>
-        {data.map((row, i) => (
-          <li key={i}>{JSON.stringify(row)}</li>
-        ))}
-      </ul>
+      <p>Website is under work. Please check back later.</p>
     </div>
   );
 }
