8238e4a44b0bfae66b1d95e32dd02770eec4014a
Eirik Buset <eirikgbu@stud.ntnu.no>
2025-03-31 14:54:05 +0100

    første versjon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.sheetbest.com/sheets/9caec6ee-9d05-425e-9a9a-b391186629d0")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Data fra Google Sheets</h1>
+      <ul>
+        {data.map((row, i) => (
+          <li key={i}>{JSON.stringify(row)}</li>
+        ))}
+      </ul>
     </div>
   );
 }
